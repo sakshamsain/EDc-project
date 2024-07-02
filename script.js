3660acb9f09e0f0ac38a663d3635f2ac46c05509
Saksham Saini <145568181+sakshamsain@users.noreply.github.com>
2024-07-02 15:01:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+/*=====drak light mode======*/
+let darkModeIcon = document.querySelector('#darkMode-icon');
 
+darkModeIcon.onclick = () => {
+  darkModeIcon.classList.toggle('bx-sun');
+  document.body.classList.toggle('dark-mode');
+};
+/*=====menu icon navbar======*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+};
