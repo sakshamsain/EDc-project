6aaf15c2f81b67a090dff0d32544ed5e0e9c7551
Saksham Saini <145568181+sakshamsain@users.noreply.github.com>
2024-07-03 14:47:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,13 @@ menuIcon.onclick = () => {
   menuIcon.classList.toggle('bx-x');
   navbar.classList.toggle('active');
 };
+/*=====scroll section active link======*/
+let sections =  document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');}}
