f033f8eccc8e293d3f03a8cb978633f85f9b7b15
Saksham Saini <145568181+sakshamsain@users.noreply.github.com>
2024-07-06 00:37:04 +0530

    Update script.js
    update window scroll code

--- script.js
+++ script.js
@@ -22,4 +22,12 @@ window.onscroll = () => {
     let top = window.scrollY;
     let offset = sec.offsetTop - 150;
     let height = sec.offsetHeight;
-    let id = sec.getAttribute('id');}}
+    let id = sec.getAttribute('id');
+if(top >= offset && top < offset + height) {
+      navlinks.forEach(links => {
+        links.classList.remove('active');
+        document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+      });
+
+    };
+  });
