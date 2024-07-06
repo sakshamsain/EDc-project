e0abf038e9f09416462f7871390adc8710e14fcc
Saksham Saini <145568181+sakshamsain@users.noreply.github.com>
2024-07-06 20:41:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ menuIcon.onclick = () => {
   menuIcon.classList.toggle('bx-x');
   navbar.classList.toggle('active');
 };
+
+
 /*=====scroll section active link======*/
 let sections =  document.querySelectorAll('section');
 let navlinks = document.querySelectorAll('header nav a');
@@ -23,7 +25,8 @@ window.onscroll = () => {
     let offset = sec.offsetTop - 150;
     let height = sec.offsetHeight;
     let id = sec.getAttribute('id');
-if(top >= offset && top < offset + height) {
+
+    if(top >= offset && top < offset + height) {
       navlinks.forEach(links => {
         links.classList.remove('active');
         document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
@@ -31,3 +34,65 @@ if(top >= offset && top < offset + height) {
 
     };
   });
+
+
+/*=====sticky navbar======*/
+let header = document.querySelector('.header');
+
+header.classList.toggle('sticky', window.scrollY > 100);
+
+
+/*=====remove menu icon navbar when click scroll======*/
+
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active');
+
+
+
+};
+
+/*=====swiper======*/
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 50,
+  loop: true,
+  grabCursor: true,
+  autoplay: {
+    delay: 5000,  // 5 second 
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+/*=====scroll reveal======*/
+ScrollReveal({
+  //reset:true,
+  distance: '80px',
+  duration: 2000,
+  delay: 200,
+});
+
+ScrollReveal().reveal('.home-content, .heading', {
+  origin: 'top'
+});
+
+ScrollReveal().reveal('.home-img img, .services-container, .portfolio-box, .testimonial-wrapper, .contact form', {
+  origin: 'bottom'
+});
+
+ScrollReveal().reveal('.home-content h1, .about-img img', {
+  origin: 'left'
+});
+
+ScrollReveal().reveal('.home-content h3, .home-content p, .about-content', {
+  origin: 'right'
+});
